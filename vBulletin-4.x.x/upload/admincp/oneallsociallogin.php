a92cff12cec782cc0c7dc4a8a8053531c0c42b5e
cschlesser <cschlesser@oneall.com>
2015-09-17 16:28:24 +0200

    Permission Fixed

--- vBulletin-4.x.x/upload/admincp/oneallsociallogin.php
+++ vBulletin-4.x.x/upload/admincp/oneallsociallogin.php
@@ -37,7 +37,7 @@ require_once ('./global.php');
 require_once (DIR . '/includes/adminfunctions_options.php');
 
 // ######################## CHECK ADMIN PERMISSIONS ######################
-if (!can_administer ('canadminvbsecurity'))
+if (!can_administer ('canadminsettings'))
 {
 	print_cp_no_permission ();
 }
